5adab9a11e12e698b17e309bc9ffbbb899189678
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2021-06-10 20:50:24 +0200

    Added game function, end game

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function computerPlay(){
 
-    let computerChoice;
     let randomComputerChoice = Math.floor(Math.random() * (3 - 0)) + 0;
-    console.log(randomComputerChoice);
-
     if (randomComputerChoice === 0){
         return("rock");
     }
@@ -13,15 +10,17 @@ function computerPlay(){
     else {
         return("scissors");
     }
-    console.log(computerPlay);
 }
 
 function roundGame(playerSelection, computerSelection){
+
     console.log("Computer choice: " + computerSelection);
     let playerInput = prompt("What do you want to choose: Rock, Paper or Scissors");
 
-    console.log(playerInput);
+    console.log("Player choice: " + playerInput);
     playerSelection = playerInput.toLowerCase();
+
+    let roundsPlayerWon = false;
 
     if (playerSelection === computerSelection){
         console.log("It's a tie");
@@ -32,12 +31,14 @@ function roundGame(playerSelection, computerSelection){
             console.log("You lose, rock vs paper");
         }
         else {
+            roundsPlayerWon = true;
             console.log("You win, rock vs scissors");
         }
     }
 
     else if (playerSelection === "paper"){
         if (computerSelection === "rock"){
+            roundsPlayerWon = true;
             console.log("You win, paper vs rock");
         }
         else{
@@ -50,14 +51,39 @@ function roundGame(playerSelection, computerSelection){
             console.log("You lose, scissors vs rock");
         }
         else{
+            roundsPlayerWon = true;
             console.log("You win, scissors vs paper");
         }
     }
-    //Return the results with return var;
+    return roundsPlayerWon;
 }
 
 const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(roundGame(playerSelection, computerSelection));
+let computerSelection;
+let scorePlayer = 0;
+let scoreComputer = 0;
 
+function game(){
+    
+    for (let i = 0; i < 5; i++){
+            console.log("New Game");   
+            computerSelection = computerPlay();
+            let playerWon = roundGame(playerSelection, computerSelection);
 
+            if (playerWon === true){
+                scorePlayer++;
+            }else{
+                scoreComputer++;
+            }
+            console.log("Player score: " + scorePlayer);
+            console.log("Computer score: " + scoreComputer);
+    }
+    if (i = 5){
+        console.log("Game finished");
+        if(scorePlayer < scoreComputer){
+            console.log("You've lost againts the computer");
+        }
+    }
+}
+
+game(playerSelection, computerSelection);
